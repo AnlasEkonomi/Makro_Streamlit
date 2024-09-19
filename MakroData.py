c303e57b2e14dc34ef0c406f31723e13c0c9c8fc
Anlaşılır Ekonomi <anlasilirekonomi@gmail.com>
2024-09-20 01:03:47 +0300

    Update MakroData.py

--- MakroData.py
+++ MakroData.py
@@ -532,6 +532,65 @@ def bist(frekans):
         veri=veri.iloc[1:]
     return veri
 
+def altın(frekans):
+    start="01-01-1987"
+    end=datetime.today().strftime("%d-%m-%Y")
+    if frekans=="Aylık":
+        veri=evdsapi.get_data(["TP.MK.LON.YTL","TP.MK.KUL.YTL"],frequency=5,aggregation_types="last",startdate=start,enddate=end)
+        veri.dropna(axis=0,inplace=True)
+        veri.columns=["Tarih","Ons Altın ($)","Gram Altın (TL)"]
+        veri["Ons Getiri Nominal (%)"]=veri["Ons Altın ($)"].pct_change()
+        veri["Ons Getiri Nominal (%)"]=veri["Ons Getiri Nominal (%)"].apply(lambda x: f"{x * 100:.2f}%")
+        veri["Gram Getiri Nominal (%)"]=veri["Gram Altın (TL)"].pct_change()
+        veri["Gram Getiri Nominal (%)"]=veri["Gram Getiri Nominal (%)"].apply(lambda x: f"{x * 100:.2f}%")
+        veri["Tarih"]=pd.to_datetime(veri["Tarih"],format="%Y-%m")
+        veri["Tarih"]=veri["Tarih"].dt.strftime("%m-%Y")
+        veri=veri.iloc[1:]
+    if frekans=="Yıllık":
+        veri=evdsapi.get_data(["TP.MK.LON.YTL","TP.MK.KUL.YTL","TP.FG.J0"],frequency=8,aggregation_types="last",startdate=start,enddate=end)
+        veri.columns=["Tarih","Ons Altın ($)","Gram Altın (TL)","TÜFE"]
+        veri["TÜFE Değişim"]=veri["TÜFE"].pct_change()
+        eski=[0.5505,0.7521,0.6877,0.6041,0.7114,0.6597,0.7108,1.2549,
+              0.7892,0.7976,0.9909,0.6973,0.6879,0.3903,0.6853,0.2975,0.1836,0.0932]
+        veri["TÜFE Değişim"].loc[:len(eski)-1]=[value for value in eski]
+        veri["Ons Getiri Nominal (%)"]=veri["Ons Altın ($)"].pct_change()
+        veri["Ons Getiri Nominal (%)"]=veri["Ons Getiri Nominal (%)"].apply(lambda x: f"{x * 100:.2f}%")
+        veri["Gram Getiri Nominal (%)"]=veri["Gram Altın (TL)"].pct_change()
+        veri["Gram Getiri Reel (%)"]=((1+veri["Gram Getiri Nominal (%)"])/(1+veri["TÜFE Değişim"])-1)
+        veri["Gram Getiri Reel (%)"]=veri["Gram Getiri Reel (%)"].apply(lambda x: f"{x * 100:.2f}%")
+        veri["Gram Getiri Nominal (%)"]=veri["Gram Getiri Nominal (%)"].apply(lambda x: f"{x * 100:.2f}%")
+        veri["Tarih"]=pd.to_datetime(veri["Tarih"],format="%Y")
+        veri["Tarih"]=veri["Tarih"].dt.strftime("%Y")
+        veri.drop(columns=["TÜFE","TÜFE Değişim"],inplace=True)
+        veri=veri.iloc[1:]
+    return veri
+
+def issizlik(tür):
+    start="01-01-2014"
+    end=datetime.today().strftime("%d-%m-%Y")
+    if tür=="Mevsimsellikten Arındırılmamış İş Gücü 15+ Nüfus":
+        veri=evdsapi.get_data(["TP.YISGUCU2.G1","TP.YISGUCU2.G2","TP.YISGUCU2.G3",
+                               "TP.YISGUCU2.G4","TP.YISGUCU2.G5","TP.YISGUCU2.G6",
+                               "TP.YISGUCU2.G7","TP.YISGUCU2.G8"],startdate=start,enddate=end)
+        veri.columns=["Tarih","Nüfus","İş Gücü","İstihdam","İşsiz","İş Gücüne Dahil Olmayan",
+                      "İş Gücüne Katılım (%)","İstihdam (%)","İşsizlik (%)"]
+        veri["İş Gücüne Katılım (%)"]=veri["İş Gücüne Katılım (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["İstihdam (%)"]=veri["İstihdam (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["İşsizlik (%)"]=veri["İşsizlik (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["Tarih"]=pd.to_datetime(veri["Tarih"],format="%Y-%m")
+        veri["Tarih"]=veri["Tarih"].dt.strftime("%m-%Y")
+    if tür=="Mevsimsellikten Arındırılmış İş Gücü 15+ Nüfus":
+        veri=evdsapi.get_data(["TP.TIG01","TP.TIG02","TP.TIG03","TP.TIG04","TP.TIG05",
+                               "TP.TIG06","TP.TIG07","TP.TIG08"],startdate=start,enddate=end)
+        veri.columns=["Tarih","Nüfus","İş Gücü","İstihdam","İşsiz","İş Gücüne Dahil Olmayan",
+                      "İş Gücüne Katılım (%)","İstihdam (%)","İşsizlik (%)"]
+        veri["İş Gücüne Katılım (%)"]=veri["İş Gücüne Katılım (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["İstihdam (%)"]=veri["İstihdam (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["İşsizlik (%)"]=veri["İşsizlik (%)"].apply(lambda x: f"{x :.2f}%")
+        veri["Tarih"]=pd.to_datetime(veri["Tarih"],format="%Y-%m")
+        veri["Tarih"]=veri["Tarih"].dt.strftime("%m-%Y")
+    return veri
+
 ##------------------------------------------------------------------------------------
 
 st.markdown("""
@@ -592,7 +651,9 @@ buttons=[
     ("TÜFE Çekirdek","button13_clicked"),
     ("İTO","button14_clicked"),
     ("Döviz","button15_clicked"),
-    ("BİST","button16_clicked")]
+    ("BİST","button16_clicked"),
+    ("Altın","button17_clicked"),
+    ("İşsizlik","button18_clicked")]
 
 def reset_buttons():
     for _, key in buttons:
@@ -1322,3 +1383,224 @@ if st.session_state.get("button16_clicked",False):
         st.plotly_chart(fig2)
 
 ##-----------------------------------------------------------------------------------------
+
+if st.session_state.get("button17_clicked",False):
+    secenek=["Aylık","Yıllık"]
+    st.markdown('<p style="font-weight:bold; color:black;">Dönem Seçiniz:</p>',unsafe_allow_html=True)
+    secim=st.radio("",secenek,index=0,horizontal=True)
+    veri=altın(secim)
+    st.dataframe(veri,hide_index=True,use_container_width=True)
+
+    if secim=="Aylık":
+        fig=go.Figure()
+        fig.add_trace(go.Scatter(
+        x=pd.to_datetime(veri["Tarih"],format="%m-%Y"),
+        y=veri["Ons Altın ($)"],
+        mode="lines",
+        name="Ons Altın ($)",
+        line=dict(color="Red")))
+
+        fig.update_layout(title={
+            "text":"Ons Altın ($)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Fiyat",
+        xaxis=dict(tickformat="%m-%Y",tickmode="linear",dtick="M5"))
+        fig.update_xaxes(tickangle=-45)
+
+        fig2=go.Figure()
+        fig2.add_trace(go.Bar(
+        x=pd.to_datetime(veri["Tarih"],format="%m-%Y"),
+        y=veri["Ons Getiri Nominal (%)"],
+        name="Getiri",
+        marker=dict(color="Blue")))
+
+        fig2.update_layout(title={
+        "text": "Ons Altın Getiri Nominal (%)", "x": 0.5, "xanchor": "center"},
+        xaxis_title="Tarih",
+        yaxis_title="Getiri",
+        xaxis=dict(tickformat="%m-%Y",tickmode="linear",dtick="M5"))
+        fig2.update_xaxes(tickangle=-45)
+        
+
+        fig3=go.Figure()
+        fig3.add_trace(go.Scatter(
+        x=pd.to_datetime(veri["Tarih"],format="%m-%Y"),
+        y=veri["Gram Altın (TL)"],
+        mode="lines",
+        name="Gram Altın (TL)",
+        line=dict(color="Red")))
+
+        fig3.update_layout(title={
+            "text":"Gram Altın (TL)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Fiyat",
+        xaxis=dict(tickformat="%m-%Y",tickmode="linear",dtick="M5"))
+        fig3.update_xaxes(tickangle=-45)
+
+        fig4=go.Figure()
+        fig4.add_trace(go.Bar(
+        x=pd.to_datetime(veri["Tarih"],format="%m-%Y"),
+        y=veri["Gram Getiri Nominal (%)"],
+        name="Getiri",
+        marker=dict(color="Blue")))
+
+        fig4.update_layout(title={
+        "text": "Gram Altın Getiri Nominal (%)", "x": 0.5, "xanchor": "center"},
+        xaxis_title="Tarih",
+        yaxis_title="Getiri",
+        xaxis=dict(tickformat="%m-%Y",tickmode="linear",dtick="M5"))
+        fig4.update_xaxes(tickangle=-45)
+
+        st.plotly_chart(fig)
+        st.plotly_chart(fig2)
+        st.plotly_chart(fig3)
+        st.plotly_chart(fig4)
+    
+    if secim=="Yıllık":
+        fig=go.Figure()
+        fig.add_trace(go.Scatter(
+        x=pd.to_datetime(veri["Tarih"],format="%Y"),
+        y=veri["Ons Altın ($)"],
+        mode="lines",
+        name="Ons Altın ($)",
+        line=dict(color="Red")))
+
+        fig.update_layout(title={
+            "text":"Ons Altın ($)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Fiyat",
+        xaxis=dict(tickformat="%Y",tickmode="linear",dtick="M5"))
+        fig.update_xaxes(tickangle=-45)
+
+
+        fig2=go.Figure()
+        fig2.add_trace(go.Scatter(
+        x=pd.to_datetime(veri["Tarih"],format="%Y"),
+        y=veri["Gram Altın (TL)"],
+        mode="lines",
+        name="Gram Altın (TL)",
+        line=dict(color="Red")))
+
+        fig2.update_layout(title={
+            "text":"Gram Altın (TL)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Fiyat",
+        xaxis=dict(tickformat="%Y",tickmode="linear",dtick="M5"))
+        fig2.update_xaxes(tickangle=-45)
+
+        fig3=go.Figure()
+        fig3.add_trace(go.Bar(
+        x=pd.to_datetime(veri["Tarih"], format="%Y"),
+        y=veri["Gram Getiri Nominal (%)"],
+        name="Nominal Getiri",
+        marker_color="Red"))
+
+        fig3.add_trace(go.Bar(
+        x=pd.to_datetime(veri["Tarih"], format="%Y"),
+        y=veri["Gram Getiri Reel (%)"],
+        name="Reel Getiri",
+        marker_color="Blue"))
+
+        fig3.update_layout(
+        title={
+            "text": "Gram Altın Getiri Nominal ve Reel (%)",
+            "x": 0.5,
+            "xanchor": "center"},
+        xaxis_title="Tarih",
+        yaxis_title="Getiri",
+        xaxis=dict(
+            tickformat="%Y",
+            tickmode="linear",
+            dtick="M12"),
+            barmode="group")
+        fig3.update_xaxes(tickangle=-45)
+
+        st.plotly_chart(fig)
+        st.plotly_chart(fig2)
+        st.plotly_chart(fig3)
+
+##--------------------------------------------------------------------------------------
+
+if st.session_state.get("button18_clicked",False):
+    secenek=["Mevsimsellikten Arındırılmamış İş Gücü 15+ Nüfus",
+             "Mevsimsellikten Arındırılmış İş Gücü 15+ Nüfus"]
+    st.markdown('<p style="font-weight:bold; color:black;">Veri Türü Seçiniz:</p>',unsafe_allow_html=True)
+    secim=st.radio("",secenek,index=0,horizontal=True)
+    veri=issizlik(secim)
+    st.dataframe(veri,hide_index=True,use_container_width=True)
+    if secim=="Mevsimsellikten Arındırılmamış İş Gücü 15+ Nüfus":
+        fig=go.Figure()
+        for column in veri.columns[1:6]:
+            fig.add_trace(go.Scatter(
+                x=veri["Tarih"],
+                y=veri[column],
+                mode="lines",
+                name=column,
+                fill="tozeroy"))
+
+        fig.update_layout(
+            title={
+                "text": "İş Gücü","x": 0.5, "xanchor": "center"},
+        xaxis_title="Tarih",
+        yaxis_title="Değer",
+        xaxis=dict(tickformat="%Y-%m",tickmode="linear"),
+        yaxis=dict(tickformat="d"))
+        fig.update_xaxes(tickangle=-45)
+
+        fig2=go.Figure()
+        for column in veri.columns[6:]:
+            fig2.add_trace(go.Bar(
+                x=veri["Tarih"],
+                y=veri[column],
+                name=column))
+        fig2.update_layout(
+            title={
+                "text": "İş Gücü (%)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Değer",
+        xaxis=dict(tickformat="%Y-%m",tickmode="linear"),
+        yaxis=dict(tickformat="d"))
+        fig2.update_xaxes(tickangle=-45)
+
+        st.plotly_chart(fig)
+        st.plotly_chart(fig2)
+
+    if secim=="Mevsimsellikten Arındırılmış İş Gücü 15+ Nüfus":
+        fig=go.Figure()
+        for column in veri.columns[1:6]:
+            fig.add_trace(go.Scatter(
+                x=veri["Tarih"],
+                y=veri[column],
+                mode="lines",
+                name=column,
+                fill="tozeroy"))
+
+        fig.update_layout(
+            title={
+                "text": "İş Gücü","x": 0.5, "xanchor": "center"},
+        xaxis_title="Tarih",
+        yaxis_title="Değer",
+        xaxis=dict(tickformat="%Y-%m",tickmode="linear"),
+        yaxis=dict(tickformat="d"))
+        fig.update_xaxes(tickangle=-45)
+
+        fig2=go.Figure()
+        for column in veri.columns[6:]:
+            fig2.add_trace(go.Bar(
+                x=veri["Tarih"],
+                y=veri[column],
+                name=column))
+        fig2.update_layout(
+            title={
+                "text": "İş Gücü (%)","x":0.5,"xanchor":"center"},
+        xaxis_title="Tarih",
+        yaxis_title="Değer",
+        xaxis=dict(tickformat="%Y-%m",tickmode="linear"),
+        yaxis=dict(tickformat="d"))
+        fig2.update_xaxes(tickangle=-45)
+
+        st.plotly_chart(fig)
+        st.plotly_chart(fig2)
+
+##--------------------------------------------------------------------------------------
+     
